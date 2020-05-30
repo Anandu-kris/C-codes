79f93c2e24674149624bd74671cdd2e77b0d1971
Anandu-kris <anandunandu1415@gmail.com>
2020-05-30 13:19:36 +0530

    C-code

---
+++ spiralmatrix.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+void main()
+{
+    int a[10][10], i, j, m, n;
+    printf("enter limit");
+    scanf("%d\n%d", &m, &n);
+    printf("\nenter elements\n");
+    for (i = 0; i < m; i++)
+        for (j = 0; j < n; j++)
+            scanf("%d", &a[i][j]);
+    printf("\n Matrix\n");
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            printf("%d\t", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\n spiral matrix\n");
+    int k = 0, l = 0;
+    while (k < m && l < n)
+    {
+        for (i = l; i < n; i++)
+        {
+            printf("%d\t", a[k][i]);
+        }
+        k++;
+        for (i = k; i < m; i++)
+        {
+            printf("%d\t", a[i][n - 1]);
+        }
+        n--;
+        if (k < m)
+        {
+            for (i = n - 1; i >= 0; i--)
+            {
+                printf("%d\t", a[m - 1][i]);
+            }
+            m--;
+        }
+        if (l < n)
+        {
+            for (i = m - 1; i > 0; i--)
+            {
+                printf("%d\t", a[i][l]);
+            }
+            l++;
+        }
+    }
+}
